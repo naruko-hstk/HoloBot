63c05a90955a7a1d7d9e0b8d93dde3787315473d
SJ Bots\結城あやの <YuukiAyano@SJBots.com>
2021-02-18 03:07:23 +0800

    Beta-0.0.2-SNAPSHOT

--- index.js
+++ index.js
@@ -24,6 +24,46 @@ holo.on('message', (msg) => {
     if (command === 'ping') {
         msg.reply(`Pong!\n延遲${Date.now() - msg.createdTimestamp}ms.`);
     }
+    if (command === 'help') {
+        const help = new Discord.MessageEmbed()
+            .setColor('#0F1D57')
+            .setTitle('Help')
+            .setDescription('Powered By結城あやの')
+            .addFields(
+                {
+                    name: '核心功能',
+                    value: 'help: 顯示此列表',
+                    inline: true
+                },
+                {
+                    name: '實用功能',
+                    value: 'ping: 測試延遲',
+                    inline: true
+                },
+                {
+                    name: '管理功能',
+                    value: 'warn: 警告使用者\nkick: 踢出使用者\nban: 封禁使用者\nmute: 水桶使用者',
+                    inline: true
+                },
+                /*{
+                    name: '',
+                    value:'',
+                    inline:false
+                },*/
+                {
+                    name: '娛樂功能',
+                    value: '@role: 提醒特定身分組活動',
+                    inline: true
+                },
+                {
+                    name: '無須前綴指令',
+                    value: '標我: 標註自己',
+                    inline: true
+                },
+            )
+            .setFooter('Copyright © 結城あやの From SJ Bots');
+        msg.channel.send(help)
+    }
 });
 
 holo.login(token);
