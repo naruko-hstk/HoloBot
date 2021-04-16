c00f6571cc56cd97a82ae3d4dac785e46c5ae22a
SJ Bots\結城あやの <YuukiAyano@SJBots.com>
2021-04-16 15:04:21 +0800

    Fix ping

--- Extensions/Utilities/ping.js
+++ Extensions/Utilities/ping.js
@@ -6,7 +6,7 @@ module.exports = {
     const ping = new Discord.MessageEmbed()
       .setColor('#0F1D57')
       .setTitle('Pong!')
-      .setDescription(`延遲${0 - (Date.now() - msg.createdTimestamp)}ms.`)
+      .setDescription(`延遲${Math.abs(Date.now() - msg.createdTimestamp)}ms.`)
       .setFooter('Copyright © 結城あやの From SJ Bots');
     msg.channel.send(ping);
   },
