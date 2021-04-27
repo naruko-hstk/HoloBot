bb7ca8c79acba3023f888765e83429f25fc411fa
SJ Bots\結城あやの <YuukiAyano@SJBots.com>
2021-04-28 04:24:10 +0900

    Add Command BanGDream

--- Extensions/SQL/bangdream.js
+++ Extensions/SQL/bangdream.js
@@ -10,70 +10,70 @@ module.exports = {
     const list = [];
     const nodata = new Discord.MessageEmbed().setColor('#0F1D57').setTitle('查無資料').setDescription(`該難度無等級${args[1]}的歌`).setFooter('Copyright © 結城あやの From SJ Bots');
     if (args[0] === 'easy' || args[0] === 'ez') {
-      await connection.query(`SELECT ID, SongNameFROM chaosjudge.bangdream WHERE EasyLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongNameFROM chaosjudge.bangdream WHERE EasyLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
           list.push(`共有**${rows.length}**首歌：\n`);
           let end = rows.length;
           for (var counter = 0; counter < end; counter++) {
-            let songs = rows[counter].ID + "：" + rows[counter].SongName;
+            let songs = rows[counter].id + "：" + rows[counter].SongName;
             list.push(songs);
           }
           msg.channel.send(list);
         }
       });
     } else if (args[0] === 'normal' || args[0] === 'nr') {
-      await connection.query(`SELECT ID, SongNameFROM chaosjudge.bangdream WHERE NormalLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongNameFROM chaosjudge.bangdream WHERE NormalLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
           list.push(`共有**${rows.length}**首歌：\n`);
           let end = rows.length;
           for (var counter = 0; counter < end; counter++) {
-            let songs = rows[counter].ID + "：" + rows[counter].SongName;
+            let songs = rows[counter].id + "：" + rows[counter].SongName;
             list.push(songs);
           }
           msg.channel.send(list);
         }
       });
     } else if (args[0] === 'hard' || args[0] === 'hd') {
-      await connection.query(`SELECT ID, SongNameFROM chaosjudge.bangdream WHERE HardLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongNameFROM chaosjudge.bangdream WHERE HardLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
           list.push(`共有**${rows.length}**首歌：\n`);
           let end = rows.length;
           for (var counter = 0; counter < end; counter++) {
-            let songs = rows[counter].ID + "：" + rows[counter].SongName;
+            let songs = rows[counter].id + "：" + rows[counter].SongName;
             list.push(songs);
           }
           msg.channel.send(list);
         }
       });
     } else if (args[0] === 'expert' || args[0] === 'ex') {
-      await connection.query(`SELECT ID, SongNameFROM chaosjudge.bangdream WHERE ExpertLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongNameFROM chaosjudge.bangdream WHERE ExpertLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
           list.push(`共有**${rows.length}**首歌：\n`);
           let end = rows.length;
           for (var counter = 0; counter < end; counter++) {
-            let songs = rows[counter].ID + "：" + rows[counter].SongName;
+            let songs = rows[counter].id + "：" + rows[counter].SongName;
             list.push(songs);
           }
           msg.channel.send(list);
         }
       });
     } else if (args[0] === 'special' || args[0] === 'sp') {
-      await connection.query(`SELECT ID, SongNameFROM chaosjudge.bangdream WHERE SpecialLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongNameFROM chaosjudge.bangdream WHERE SpecialLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
           list.push(`共有**${rows.length}**首歌：\n`);
           let end = rows.length;
           for (var counter = 0; counter < end; counter++) {
-            let songs = rows[counter].ID + "：" + rows[counter].SongName;
+            let songs = rows[counter].id + "：" + rows[counter].SongName;
             list.push(songs);
           }
           msg.channel.send(list);
