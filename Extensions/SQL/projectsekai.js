e3cc1ff9743e99dffdf7464bd07d81a7205ec9b9
SJ Bots\結城あやの <YuukiAyano@SJBots.com>
2021-04-29 11:37:36 +0900

    Add Command ProjectSekai

--- Extensions/SQL/projectsekai.js
+++ Extensions/SQL/projectsekai.js
@@ -12,7 +12,7 @@ module.exports = {
     const nodata = new Discord.MessageEmbed().setColor('#0F1D57').setTitle('查無資料').setDescription(`該難度無等級${args[1]}的歌`).setFooter('Copyright © 結城あやの From SJ Bots');
     // const play = new Discord.MessageEmbed().setColor('#0F1D57').setTitle('想打嗎？').setDescription('[點我線上打歌](https://player.banground.fun/)\n輸入上面查到的ID即可打歌').setFooter('Copyright © 結城あやの From SJ Bots');
     if (args[0] === 'easy' || args[0] === 'ez') {
-      await connection.query(`SELECT id, SongName FROM chaosjudge.bangdream WHERE EasyLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongName FROM chaosjudge.projectsekai WHERE EasyLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
@@ -27,7 +27,7 @@ module.exports = {
         }
       });
     } else if (args[0] === 'normal' || args[0] === 'nr' || args[0] === 'basic' || args[0] === 'bsc') {
-      await connection.query(`SELECT id, SongName FROM chaosjudge.bangdream WHERE NormalLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongName FROM chaosjudge.projectsekai WHERE NormalLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
@@ -42,7 +42,7 @@ module.exports = {
         }
       });
     } else if (args[0] === 'hard' || args[0] === 'hd' || args[0] === 'advance' || args[0] === 'adv') {
-      await connection.query(`SELECT id, SongName FROM chaosjudge.bangdream WHERE HardLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongName FROM chaosjudge.projectsekai WHERE HardLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
@@ -57,7 +57,7 @@ module.exports = {
         }
       });
     } else if (args[0] === 'expert' || args[0] === 'ex' || args[0] === 'exp') {
-      await connection.query(`SELECT id, SongName FROM chaosjudge.bangdream WHERE ExpertLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongName FROM chaosjudge.projectsekai WHERE ExpertLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
@@ -72,7 +72,7 @@ module.exports = {
         }
       });
     } else if (args[0] === 'master' || args[0] === 'ma' || args[0] === 'mas') {
-      await connection.query(`SELECT id, SongName FROM chaosjudge.bangdream WHERE SpecialLevel = ${parseInt(args[1])};`, (err, rows) => {
+      await connection.query(`SELECT id, SongName FROM chaosjudge.projectsekai WHERE SpecialLevel = ${parseInt(args[1])};`, (err, rows) => {
         if (err) throw err;
         if (rows.length < 1) msg.channel.send(nodata);
         else {
